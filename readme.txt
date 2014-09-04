b09a713b167444db3a064521e5f177d3bc4bbf22
s3bubble <s3bubble@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 20:45:05 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/s3bubble-amazon-s3-video-popup/trunk@980512 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,145 @@
+﻿=== S3Bubble Amazon S3 Video And Audio Streaming Service ===
+Contributors: Samuel East
+Donate link: https://s3bubble.com
+Tags: S3Bubble, AmazonS3, Amazon S3, HTML5 Player, Video, VideoPlayer, MediaPlayer, MediaStreaming, Media-Streaming, Media Streaming, VideoStreaming, Video-streaming, Video Streaming, MediaStream, Advert, Advertising
+Requires at least: 2.0
+Tested up to: 3.9.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+S3Bubble offers simple, secure media streaming from Amazon S3 to Wordpress all in just 3 simple steps!
+
+== Description ==
+
+Now with WYSIWYG buttons [View more information on this](https://s3bubble.com/need-extra-video-security-wysiwyg-shortcode-generate-buttons/ "View more information on this")
+
+**Now Compatible With Media Elements JS.**
+
+* Step 1 - Link your Amazon S3 Account to S3Bubble
+* Step 2 - Add Audio, Video and Image files through S3Bubble Admin Tools
+* Step 3 - Embed the files in our responsive player on your website
+
+Download the plugin or Sign Up for an account at [S3Bubble](https://s3bubble.com/auth/?action=register&utm_source=wordpress&utm_medium=link&utm_campaign=wordpresspluginpage "S3bubble")
+
+= Why Choose S3Bubble: =
+
+**Embed in one click, stream to any device.**
+Add a file to a widget and embed the link into your site in a few clicks. S3Bubble handles the rest. Generating thumbnails. encoding audio and making sure you're content is ready for the web.
+
+**Secure Online Media Streaming.**
+Youtube is great but if you’d rather get users to pay for your content S3Bubble may be for you. Combined with a membership paywall you can professionally stream images, audio and video to your members hitch free.
+
+**You could save a fortune in media streaming.**
+Hosting video, galleries and audio files is heavyweight work. S3Bubble adds powerful functionality to your existing Amazon S3 account, so you can forget about hosting issues. Because of this, you only ever pay for what you use.
+
+Download the plugin or Sign Up for an account at [S3Bubble](https://s3bubble.com/auth/?action=register&utm_source=wordpress&utm_medium=link&utm_campaign=wordpresspluginpage "S3bubble")
+
+[youtube https://www.youtube.com/watch?v=GcTLSOvddaM]
+
+= Features: =
+* HTML 5 Player (Supports iOS, Web, Android, Windows Phone etc.)
+* Tested with IE (7-10), Chrome, Safari and Firefox
+* Responsive player, resizes based on fluid grids
+* Range of Player options
+* Single Video Player
+* Friendly support forum - support from our Developers and current members
+
+Download the plugin or Sign Up for an account at [S3Bubble](https://s3bubble.com/auth/?action=register&utm_source=wordpress&utm_medium=link&utm_campaign=wordpresspluginpage "S3bubble")
+
+== Installation ==
+
+Once you’ve installed the plug-in, the S3 Bubble Tutorial will guide you along every step of the way, but if you need any extra help you can visit our friendly [support forum](https://s3bubble.com/forums/?utm_source=wordpress&utm_medium=link&utm_campaign=s3bubbleforum "problems").
+
+Once you’ve installed the S3Bubble plug-in, you’ll need to create an S3Bubble Account [(click here to do it now)](https://s3bubble.com/auth/?action=register&utm_source=wordpress&utm_medium=link&utm_campaign=wordpresspluginpage "s3bubble sign up").
+
+= Then there’s just 3 steps to get you started: =
+
+* Step 1 - Link your Amazon S3 Account to S3Bubble
+* Step 2 - Add Audio, Video and Image files through S3Bubble Admin Tools
+* Step 3 - Embed the files in our responsive player on your website
+
+Remember if you need any extra help you can visit our friendly [support forum](https://s3bubble.com/forums/?utm_source=wordpress&utm_medium=link&utm_campaign=s3bubbleforum "problems").
+
+[youtube https://www.youtube.com/watch?v=GcTLSOvddaM]
+
+= These are auto generated within your s3bubble admin =
+
+**Audio Playlist Shortcode Example.**
+`[s3bubbleAudio bucket="enter-your-bucket" folder="enter-your-bucket-folder"]`
+
+**Audio Single Player Shortcode Example.**
+`[s3bubbleAudioSingle bucket="enter-your-bucket" track="enter-your-track-name"]`
+
+**Video Playlist Shortcode Example.**
+`[s3bubbleVideo bucket="enter-your-bucket" folder="enter-your-bucket-folder"]`
+
+**Video Single Player Shortcode Example.**
+`[s3bubbleVideoSingle bucket="enter-your-bucket" track="enter-your-track-name"]`
+
+= Params - these can be added to the shortcode =
+* bucket: //your amazon bucket
+* folder: //your amazon s3 folder
+* autoplay: //true or false 
+* height: //height of the player
+* playlist: //hidden
+* style: plain //hide bar on single audio player 
+
+== Frequently Asked Questions ==
+
+Once you’ve installed the plug-in, the S3 Bubble Tutorial will guide you along every step of the way, but if you need any extra help you can visit our friendly [support forum](https://s3bubble.com/forums/?utm_source=wordpress&utm_medium=link&utm_campaign=s3bubbleforum "problems").
+
+**What is S3Bubble?**
+S3Bubble is a media streaming service that allows you to serve video, audio and images from your Amazon S3 account to a widget that can be embedded within wordpress.
+
+**How much does it cost?**
+[You can sign up for a 28 day free trial of the service here >](https://s3bubble.com/auth/?action=register&utm_source=wordpress&utm_medium=link&utm_campaign=wordpresspluginpage "S3bubble")
+
+S3Bubble costs from $5.99-$7.99 p/month plus the costs of your hosting with Amazon S3 (detailed below). For this you get an unbranded player, unlimited media streaming and all the back end tools to manage your files.
+
+**What’s Amazon S3?**
+Amazon S3 is a cloud storage platform, that charges on a per-usage basis. Essentially, a very cheap way to store and share large amounts of data. [Check their pricing out here >](http://aws.amazon.com/s3/pricing/ "S3bubble Amazon S3 Pricing")
+
+**How does the plug-in work?**
+Much the same as any other Wordpress plug-in. Simply install it as normal. [Then create your S3Bubble Account](https://s3bubble.com/auth/?action=register&utm_source=wordpress&utm_medium=link&utm_campaign=wordpresspluginpage "S3bubble Sign Up")
+
+**What are the embeddable media players?**
+The media players are fully responsive and tested with all the major mobile platforms and web browsers. [You can see a full range of them here >](https://s3bubble.com/category/embed-players/ "S3bubble Embed Players")
+
+**What about the Mobile App?**
+For premium users the mobile app offers a way to record, upload and manage their content. The app is available on both iOS and Android. 
+
+**Who are S3Bubble?**
+S3Bubble is made by a small but passionate team based [here](http://www.welshice.org/ "S3bubble Welsh ICE"), in Wales in the UK.
+We love media and technology, so we decided to make it easier for people to share their content. If you want to get in touch, you can find us here on [Twitter >](https://twitter.com/s3bubble "S3bubble Twitter")
+
+**Got another Question?**
+Once you’ve installed the plug-in, the S3 Bubble Tutorial will guide you along every step of the way, but if you need any extra help you can visit our friendly [support forum](https://s3bubble.com/forums/?utm_source=wordpress&utm_medium=link&utm_campaign=s3bubbleforum "problems").
+
+== Screenshots ==
+
+1. Record video from mobile and directly upload
+2. Enter your S3Bubble email and password to create a sync
+3. Sync your Amazon S3 Keys with S3Bubble Admin
+4. Upload your file ready to stream
+5. All your file video,audio and image all stored securely within buckets
+
+== Changelog ==
+
+= 0.1 =
+
+Issue with Android phone
+
+Security bug - NOW FIXED
+
+Force download - NOW FIXED
+
+Currently issues with flash playback off mp3 files in firefox - NOW FIXED
+
+[Any Problems Add Here](https://s3bubble.com/forums/?utm_source=wordpress&utm_medium=link&utm_campaign=s3bubbleforum "problems")
+
+== Upgrade Notice ==
+
+= 1.0 =
+
+Upgrade notices for S3Bubble.
---
+++ s3bubble-video-popup.php
@@ -0,0 +1,322 @@
+<?php
+/*
+Plugin Name: S3Bubble Video Popup
+Plugin URI: https://www.s3bubble.com/
+Description: S3Bubble offers simple, secure media streaming from Amazon S3 to WordPress and adding your very own adverts. In just 4 simple steps. 
+Version: 0.1
+Author: S3Bubble
+Author URI: https://s3bubble.com/
+License: GPL2
+*/ 
+ 
+/*  Copyright YEAR  Samuel East  (email : mail@samueleast.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/ 
+
+
+if (!class_exists("s3bubble_video_popup")) {
+	class s3bubble_video_popup {
+
+		/*
+		 * Class properties
+		 * @author sameast
+		 * @params noen
+		 */ 
+        public  $s3bubble_video_popup_key          = 'mWWMeyGgW';
+		public  $s3bubble_video_popup_width        = 600;
+		public  $s3bubble_video_popup_button_text  = 'Featured Video Powered by S3Bubble';
+		public  $s3bubble_button_position          = 'bottom';
+		public  $s3bubble_video_popup_link         = 'https://s3bubble.com/';
+		public  $s3bubble_video_popup_link_text    = 'S3Bubble';
+		public  $version                           = 1;
+		
+		/*
+		 * Constructor method to intiat the class
+		 * @author sameast
+		 * @params none
+		 */ 
+		function s3bubble_video_popup(){
+
+			/*
+			 * Add default option to database
+			 * @author sameast
+			 * @params none
+			 */ 
+			add_option("s3bubble_video_popup_key", $this->s3bubble_video_popup_key);
+			add_option("s3bubble_video_popup_width", $this->s3bubble_video_popup_width);
+			add_option("s3bubble_video_popup_button_text", $this->s3bubble_video_popup_button_text);
+			add_option("s3bubble_button_position", $this->s3bubble_button_position);
+			add_option("s3bubble_video_popup_link", $this->s3bubble_video_popup_link);
+			add_option("s3bubble_video_popup_link_text", $this->s3bubble_video_popup_link_text);
+
+			/*
+			 * Run the add admin menu class
+			 * @author sameast
+			 * @params none
+			 */ 
+			add_action('admin_menu', array( $this, 's3bubble_video_popup_admin_menu' ));
+			
+			/*
+			 * Add css to the header of the document
+			 * @author sameast
+			 * @params none
+			 */ 
+			add_action( 'wp_head', array( $this, 's3bubble_video_popup_css' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 's3bubble_video_popup_javascript' ), 11 );
+			
+			/*
+			 * Add css to the wordpress admin document
+			 * @author sameast
+			 * @params none
+			 */ 
+			add_action( 'admin_head', array( $this, 's3bubble_video_popup_css_admin' ) );
+			
+			/*
+			 * Add javascript to the frontend footer connects to wp_footer
+			 * @author sameast
+			 * @params none
+			 */ 
+			add_action( 'admin_footer', array( $this, 's3bubble_video_popup_javascript_admin' ) );
+			
+			/*
+			 * Grab the popup html
+			 */ 
+			add_action( 'wp_ajax_s3bubble_video_popup_internal_ajax', array( $this, 's3bubble_video_popup_internal_ajax' ) );
+			add_action('wp_ajax_nopriv_s3bubble_video_popup_internal_ajax', array( $this, 's3bubble_video_popup_internal_ajax' ) ); 
+			
+		}
+
+		/*
+		* Adds the menu item to the wordpress admin
+		* @author sameast
+		* @none
+		*/ 
+        function s3bubble_video_popup_internal_ajax(){
+			$s3bubble_video_popup_key	      = get_option("s3bubble_video_popup_key");
+            $s3bubble_video_popup_link	      = get_option("s3bubble_video_popup_link");
+			$s3bubble_video_popup_link_text   = get_option("s3bubble_video_popup_link_text");
+			$data = array(
+						'link'=> $s3bubble_video_popup_link,
+              			'linktext'=> $s3bubble_video_popup_link_text,
+              			);
+			  
+            echo '<div id="s3bubble-frame">
+					<a id="s3bubble-frame-close" href="#">Close</a>
+						<div id="s3bubble-frame-wrap">
+							<iframe id="s3bubble-iframe-load" style="width:100%;" src="//s3bubble.com/popup?v=' . $s3bubble_video_popup_key . '&' . http_build_query($data, '', '&amp;') . '" marginheight="0" marginwidth="0" frameborder="0" allowtransparency="true" webkitAllowFullScreen="true" mozallowfullscreen="true" allowFullScreen="true" />
+						</div>
+				</div>';
+			die();	
+		}		
+        
+		/*
+		* Adds the menu item to the wordpress admin
+		* @author sameast
+		* @none
+		*/ 
+        function s3bubble_video_popup_admin_menu(){	
+			add_menu_page( 's3bubble_video_popup', 'S3Bubble Popup', 'manage_options', 's3bubble_video_popup', array($this, 's3bubble_video_popup_admin'), plugins_url('assets/images/s3bubblelogo.png',__FILE__ ) );
+    	}
+        
+		/*
+		* Add css to wordpress admin to run colourpicker
+		* @author sameast
+		* @none
+		*/ 
+		function s3bubble_video_popup_css_admin(){
+			wp_register_style( 's3bubble.video.popup.admin', plugins_url('assets/css/s3bubble.video.popup.admin.css', __FILE__) );
+			wp_enqueue_style('s3bubble.video.popup.admin');
+			wp_register_style( 's3bubble.video.popup.plugin', plugins_url('assets/css/s3bubble.video.popup.plugin.css', __FILE__) );
+			wp_enqueue_style('s3bubble.video.popup.plugin');
+		}
+		
+        /*
+		* Add javascript to the admin header
+		* @author sameast
+		* @none
+		*/ 
+		function s3bubble_video_popup_javascript_admin(){
+
+		} 
+		
+		/*
+		* Add css ties into wp_head() function
+		* @author sameast
+		* @params none
+        */ 
+		function s3bubble_video_popup_css(){
+			$width = get_option("s3bubble_video_popup_width");
+			wp_register_style( 's3bubble.button', plugins_url('assets/css/s3bubble.button.css', __FILE__), array(), $this->version );
+			wp_enqueue_style('s3bubble.button');
+			wp_register_style( 's3bubble.frame', plugins_url('assets/css/s3bubble.frame.css', __FILE__), array(), $this->version );
+			wp_enqueue_style('s3bubble.frame');
+			echo "<style type='text/css'>#s3bubble-frame{max-width: " . (empty($width) ? 600 : $width ) . "px}</style>";
+		}
+		
+		/*
+		* Add javascript to the footer connect to wp_footer()
+		* @author sameast
+		* @none
+		*/ 
+		function s3bubble_video_popup_javascript(){
+           if (!is_admin()) {
+           	    $text     = get_option("s3bubble_video_popup_button_text");
+			    $position = get_option("s3bubble_button_position");
+	            wp_deregister_script( 'jquery' );
+	            wp_register_script( 'jquery', '//code.jquery.com/jquery-1.11.0.min.js', false, null);
+	            wp_enqueue_script('jquery');
+	            wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-1.2.1.min.js', false, null);
+	            wp_enqueue_script('jquery-migrate');
+				wp_register_script( 'button.min', plugins_url('assets/js/button.js',__FILE__ ), array(), $this->version );
+	            wp_enqueue_script('button.min');
+				echo '<script type="text/javascript">
+				/* <![CDATA[ */
+				var s3bubblePopup = {
+					"text":"' . (empty($text) ? 'Video Tutorial' : $text ) . '", 
+					"position":"' . (empty($text) ? 'right' : $position ) . '", 
+					"ajax":"' . admin_url('admin-ajax.php') . '"
+					};
+				/* ]]> */
+				</script>';
+            } 
+		}
+        
+		/*
+		* Add javascript to the footer connect to wp_footer()
+		* @author sameast
+		* @none
+		*/ 
+		function s3bubble_video_popup_admin(){	
+			if ( isset($_POST['submit']) ) {
+				$nonce = $_REQUEST['_wpnonce'];
+				if (! wp_verify_nonce($nonce, 'isd-updatesettings') ) die('Security check failed'); 
+				if (!current_user_can('manage_options')) die(__('You cannot edit the video popup options.'));
+				check_admin_referer('isd-updatesettings');	
+				// Get our new option values
+				$s3bubble_video_popup_key	      = $_POST['s3bubble_video_popup_key'];
+				$s3bubble_video_popup_width	      = $_POST['s3bubble_video_popup_width'];
+				$s3bubble_video_popup_button_text = $_POST['s3bubble_video_popup_button_text'];
+				$s3bubble_button_position         = $_POST['s3bubble_button_position'];
+				$s3bubble_video_popup_link        = $_POST['s3bubble_video_popup_link'];
+				$s3bubble_video_popup_link_text   = $_POST['s3bubble_video_popup_link_text'];
+				
+			    // Update the DB with the new option values
+				update_option("s3bubble_video_popup_key", $s3bubble_video_popup_key);
+				update_option("s3bubble_video_popup_width", $s3bubble_video_popup_width);
+				update_option("s3bubble_video_popup_button_text", $s3bubble_video_popup_button_text);
+				update_option("s3bubble_button_position", $s3bubble_button_position);
+				update_option("s3bubble_video_popup_link", $s3bubble_video_popup_link);
+				update_option("s3bubble_video_popup_link_text", $s3bubble_video_popup_link_text);
+			}
+
+			$s3bubble_video_popup_key	      = get_option("s3bubble_video_popup_key");
+			$s3bubble_video_popup_width	      = get_option("s3bubble_video_popup_width");
+			$s3bubble_video_popup_button_text = get_option("s3bubble_video_popup_button_text");
+			$s3bubble_button_position         = get_option("s3bubble_button_position");
+			$s3bubble_video_popup_link        = get_option("s3bubble_video_popup_link");
+			$s3bubble_video_popup_link_text   = get_option("s3bubble_video_popup_link_text");
+		?>
+		<div class="wrap">
+			<div id="icon-options-general" class="icon32"></div>
+			<h2>S3Bubble Amazon S3 Popup Video</h2>
+			<div class="metabox-holder has-right-sidebar">
+				<div class="inner-sidebar" style="width:50%">
+					<div class="postbox">
+						<h3><span>PLEASE WATCH TUTORIAL VIDEO</span></h3>
+						<div class="inside">
+							<iframe width="100%" height="315" src="//www.youtube.com/embed/GcTLSOvddaM" frameborder="0" allowfullscreen></iframe>
+						</div> 
+					</div>
+
+				</div>
+				<div id="post-body">
+					<div id="post-body-content" style="margin-right: 51%;">
+						<div class="postbox">
+							<h3><span>Please sign up for an account at <a href="https://s3bubble.com/auth/?action=register&utm_source=wordpress&utm_medium=link&utm_campaign=pluginpage" target="_blank">https://s3bubble.com</a> you will need to use the username and email you signed up with.</span></h3>
+							<div class="inside">
+								<form action="" method="post" id="isd-config" style="overflow: hidden;">
+								    <table class="form-table">
+								      <?php if (function_exists('wp_nonce_field')) { wp_nonce_field('isd-updatesettings'); } ?>
+								      <tr>
+								        <th scope="row" valign="top"><label for="s3bubble_video_popup_key">S3Bubble Video ID:</label></th>
+								        <td><input type="text" name="s3bubble_video_popup_key" id="s3bubble_video_popup_key" class="regular-text" value="<?php echo $s3bubble_video_popup_key; ?>"/>
+								        	<br />
+								        	<span class="description">Video ID can be found in S3Bubble admin.</span>
+								        </td>
+								      </tr>  
+								      <tr>
+								        <th scope="row" valign="top"><label for="s3bubble_video_popup_button_text">Button Text:</label></th>
+								        <td><input type="text" name="s3bubble_video_popup_button_text" id="s3bubble_video_popup_button_text" class="regular-text" value="<?php echo $s3bubble_video_popup_button_text; ?>"/>
+								        	<br />
+								        	<span class="description">Text for the button</span>
+								        </td>
+								      </tr> 
+								      <tr>
+								        <th scope="row" valign="top"><label for="s3bubble_video_popup_width">Video Width:</label></th>
+								        <td><input type="text" name="s3bubble_video_popup_width" id="s3bubble_video_popup_width" class="regular-text" value="<?php echo $s3bubble_video_popup_width; ?>"/>
+								        	<br />
+								        	<span class="description">Do not add px</span>
+								        </td>
+								      </tr>
+								      <tr>
+								        <th scope="row" valign="top"><label for="s3bubble_button_position">Button Position:</label></th>
+								        <td><select name="s3bubble_button_position" id="s3bubble_button_position">
+								            <option value="<?php echo $s3bubble_button_position; ?>"><?php echo $s3bubble_button_position; ?></option>
+								            <option value="left">left</option>
+								            <option value="right">right</option>
+								            <option value="bottom">bottom</option>
+								          </select>
+								          <br />
+								          <span class="description">Change button position.</p></td>
+								      </tr>
+								      <tr>
+								        <th scope="row" valign="top"><label for="s3bubble_video_popup_link">Video Link:</label></th>
+								        <td><input type="text" name="s3bubble_video_popup_link" id="s3bubble_video_popup_link" class="regular-text" value="<?php echo $s3bubble_video_popup_link; ?>"/>
+								        	<br />
+								        	<span class="description">Optional leave blank for no link</span>
+								        </td>
+								      </tr>
+								      <tr>
+								        <th scope="row" valign="top"><label for="s3bubble_video_popup_link_text">Video Link Text:</label></th>
+								        <td><input type="text" name="s3bubble_video_popup_link_text" id="s3bubble_video_popup_link_text" class="regular-text" value="<?php echo $s3bubble_video_popup_link_text; ?>"/>
+								        	<br />
+								        	<span class="description">Video link text url must be supplied</span>
+								        </td>
+								      </tr>  
+								    </table>
+								    <br/>
+								    <span class="description">To remove branding please upgrade your account. <a href="https://s3bubble.com/pricing/" target="_blank">Upgrade</a></p>
+								    <span class="submit" style="border: 0;">
+								    <input type="submit" name="submit" class="button button-primary button-hero" value="Save Settings" />
+								    </span>
+								  </form>
+							</div><!-- .inside -->
+						</div>
+					</div> <!-- #post-body-content -->
+				</div> <!-- #post-body -->
+			</div> <!-- .metabox-holder -->
+		</div> <!-- .wrap -->
+		<?php	
+       } 
+
+	}
+	/*
+	* Initiate the class
+	* @author sameast
+	* @none
+	*/ 
+	$s3bubble_video_popup = new s3bubble_video_popup();
+
+} //End Class s3audible
